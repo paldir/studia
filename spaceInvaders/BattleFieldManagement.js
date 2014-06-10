d97004eb49eebeac9a5b54267c6358f5d3fe7341
Patryk <patrickus@wp.pl>
2014-06-10 23:16:22 +0200

    Testowanie wyswietlanie

---
+++ spaceInvaders/BattleFieldManagement.js
@@ -0,0 +1,34 @@
+﻿function BattleFieldManagement() {
+    this.matrixOfBattleField = new Array(272);
+
+    for (var i = 0; i < this.matrixOfBattleField.length; i++) {
+        this.matrixOfBattleField[i] = new Array(272);
+
+        for (var j = 0; j < this.matrixOfBattleField[i].length; j++) {
+            this.matrixOfBattleField[i][j] = 0;
+        }
+    }
+
+    this.FillMatrix = function () {
+        this.matrixOfBattleField[104][101] = 1;
+        this.matrixOfBattleField[105][101] = 1;
+        this.matrixOfBattleField[106][101] = 1;
+        this.matrixOfBattleField[106][102] = 1;
+        this.matrixOfBattleField[104][103] = 1;
+        this.matrixOfBattleField[105][103] = 1;
+        this.matrixOfBattleField[106][103] = 1;
+        this.matrixOfBattleField[101][104] = 1;
+        this.matrixOfBattleField[102][104] = 1;
+        this.matrixOfBattleField[103][104] = 1; 
+        this.matrixOfBattleField[104][104] = 1;
+        this.matrixOfBattleField[105][104] = 1;
+        this.matrixOfBattleField[106][104] = 1;
+        this.matrixOfBattleField[104][105] = 1;
+        this.matrixOfBattleField[105][105] = 1;
+        this.matrixOfBattleField[106][105] = 1;
+        this.matrixOfBattleField[106][106] = 1;
+        this.matrixOfBattleField[104][107] = 1;
+        this.matrixOfBattleField[105][107] = 1;
+        this.matrixOfBattleField[106][107] = 1;
+    }
+}
---
+++ spaceInvaders/Drawing.js
@@ -0,0 +1,25 @@
+﻿function DrawBattleField(idOfCanvas, matrixOfBattleField) {
+    var canvasOfBattleField = document.getElementById(idOfCanvas);
+    var widthOfPoint = canvasOfBattleField.width / matrixOfBattleField.length;
+    var contextOfCanvas = canvasOfBattleField.getContext('2d');
+
+    for (var i = 0; i < matrixOfBattleField.length; i++) {
+        for (var j = 0; j < matrixOfBattleField[i].length; j++) {
+            switch (matrixOfBattleField[i][j]) {
+                case 2:
+                    color = 'red';
+                    break;
+                case 1:
+                    color = 'green';
+                    break;
+                default:
+                    color = 'black';
+            }
+
+            contextOfCanvas.beginPath();
+            contextOfCanvas.rect(i, j, widthOfPoint, widthOfPoint);
+            contextOfCanvas.fillStyle = color;
+            contextOfCanvas.fill();
+        }
+    }
+}
