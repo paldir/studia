a104011ad6b71a63cbfac510fdd42a7779743ca1
Patryk <patrickus@wp.pl>
2014-06-11 17:51:00 +0200

    Plansza poczatkowa

--- spaceInvaders/BattleFieldManagement.js
+++ spaceInvaders/BattleFieldManagement.js
@@ -1,34 +1,15 @@
-﻿function BattleFieldManagement() {
-    this.matrixOfBattleField = new Array(272);
+﻿/// <reference path="ShipsAndMissiles.js" />
+function BattleFieldManagement() {
+    this.invaders = new Array(55);
+    this.hero = new Hero();
 
-    for (var i = 0; i < this.matrixOfBattleField.length; i++) {
-        this.matrixOfBattleField[i] = new Array(272);
+    var numberOfRow = 1;
 
-        for (var j = 0; j < this.matrixOfBattleField[i].length; j++) {
-            this.matrixOfBattleField[i][j] = 0;
-        }
-    }
+    for (var i = 0; i < this.invaders.length; i++) {
+        var numberOfInvader = i % 11 + 1;
+        this.invaders[i] = new Invader(numberOfInvader, numberOfRow);
 
-    this.FillMatrix = function () {
-        this.matrixOfBattleField[104][101] = 1;
-        this.matrixOfBattleField[105][101] = 1;
-        this.matrixOfBattleField[106][101] = 1;
-        this.matrixOfBattleField[106][102] = 1;
-        this.matrixOfBattleField[104][103] = 1;
-        this.matrixOfBattleField[105][103] = 1;
-        this.matrixOfBattleField[106][103] = 1;
-        this.matrixOfBattleField[101][104] = 1;
-        this.matrixOfBattleField[102][104] = 1;
-        this.matrixOfBattleField[103][104] = 1; 
-        this.matrixOfBattleField[104][104] = 1;
-        this.matrixOfBattleField[105][104] = 1;
-        this.matrixOfBattleField[106][104] = 1;
-        this.matrixOfBattleField[104][105] = 1;
-        this.matrixOfBattleField[105][105] = 1;
-        this.matrixOfBattleField[106][105] = 1;
-        this.matrixOfBattleField[106][106] = 1;
-        this.matrixOfBattleField[104][107] = 1;
-        this.matrixOfBattleField[105][107] = 1;
-        this.matrixOfBattleField[106][107] = 1;
+        if (numberOfInvader == 11)
+            numberOfRow++;
     }
 }
--- spaceInvaders/Drawing.js
+++ spaceInvaders/Drawing.js
@@ -1,25 +1,57 @@
-﻿function DrawBattleField(idOfCanvas, matrixOfBattleField) {
-    var canvasOfBattleField = document.getElementById(idOfCanvas);
-    var widthOfPoint = canvasOfBattleField.width / matrixOfBattleField.length;
-    var contextOfCanvas = canvasOfBattleField.getContext('2d');
+﻿function Drawing(idOfCanvas) {
+    this.canvasOfBattleField = document.getElementById(idOfCanvas);
+    this.cellWidth = this.canvasOfBattleField.width / 44;
+    this.contextOfCanvas = this.canvasOfBattleField.getContext('2d');
 
-    for (var i = 0; i < matrixOfBattleField.length; i++) {
-        for (var j = 0; j < matrixOfBattleField[i].length; j++) {
-            switch (matrixOfBattleField[i][j]) {
-                case 2:
-                    color = 'red';
-                    break;
-                case 1:
-                    color = 'green';
-                    break;
-                default:
-                    color = 'black';
+    this.DrawInvaders = function (invaders) {
+        for (var i = 0; i < invaders.length; i++) {
+            if (!invaders[i].destroyed) {
+                this.contextOfCanvas.beginPath();
+                this.contextOfCanvas.arc(invaders[i].locationX * this.cellWidth + 0.5 * this.cellWidth, invaders[i].locationY * this.cellWidth + 0.5 * this.cellWidth, 0.25 * this.cellWidth, 2 * Math.PI, false);
+                this.contextOfCanvas.fillStyle = 'red';
+                this.contextOfCanvas.fill();
+                this.contextOfCanvas.moveTo(invaders[i].locationX * this.cellWidth, invaders[i].locationY * this.cellWidth + 0.5 * this.cellWidth);
+                this.contextOfCanvas.lineTo(invaders[i].locationX * this.cellWidth + this.cellWidth, invaders[i].locationY * this.cellWidth + 0.5 * this.cellWidth);
+                this.contextOfCanvas.moveTo(invaders[i].locationX * this.cellWidth, invaders[i].locationY * this.cellWidth);
+                this.contextOfCanvas.lineTo(invaders[i].locationX * this.cellWidth, invaders[i].locationY * this.cellWidth + this.cellWidth);
+                this.contextOfCanvas.moveTo(invaders[i].locationX * this.cellWidth + this.cellWidth, invaders[i].locationY * this.cellWidth);
+                this.contextOfCanvas.lineTo(invaders[i].locationX * this.cellWidth + this.cellWidth, invaders[i].locationY * this.cellWidth + this.cellWidth);
+                this.contextOfCanvas.lineWidth = 3;
+                this.contextOfCanvas.strokeStyle = 'red';
+                this.contextOfCanvas.stroke();
             }
+        }
+    }
 
-            contextOfCanvas.beginPath();
-            contextOfCanvas.rect(i, j, widthOfPoint, widthOfPoint);
-            contextOfCanvas.fillStyle = color;
-            contextOfCanvas.fill();
+    this.DrawHero = function (hero) {
+        this.contextOfCanvas.beginPath();
+        this.contextOfCanvas.moveTo(hero.locationX * this.cellWidth, hero.locationY * this.cellWidth + this.cellWidth);
+        this.contextOfCanvas.lineTo(hero.locationX * this.cellWidth + this.cellWidth, hero.locationY * this.cellWidth + this.cellWidth);
+        this.contextOfCanvas.lineTo(hero.locationX * this.cellWidth + this.cellWidth, hero.locationY * this.cellWidth + 0.5 * this.cellWidth);
+        this.contextOfCanvas.lineTo(hero.locationX * this.cellWidth + 0.75 * this.cellWidth, hero.locationY * this.cellWidth + 0.75 * this.cellWidth);
+        this.contextOfCanvas.lineTo(hero.locationX * this.cellWidth + 0.5 * this.cellWidth, hero.locationY * this.cellWidth);
+        this.contextOfCanvas.lineTo(hero.locationX * this.cellWidth + 0.25 * this.cellWidth, hero.locationY * this.cellWidth + 0.75 * this.cellWidth);
+        this.contextOfCanvas.lineTo(hero.locationX * this.cellWidth, hero.locationY * this.cellWidth + 0.5 * this.cellWidth);
+        this.contextOfCanvas.closePath();
+        this.contextOfCanvas.fillStyle = 'green';
+        this.contextOfCanvas.fill();
+    }
+
+    this.DrawGrid = function () {
+        for (var i = 0; i < 44; i++) {
+            this.contextOfCanvas.beginPath();
+            this.contextOfCanvas.moveTo(i * this.cellWidth, 0);
+            this.contextOfCanvas.lineTo(i * this.cellWidth, this.canvasOfBattleField.height);
+            this.contextOfCanvas.strokeStyle = 'white';
+            this.contextOfCanvas.stroke();
+        }
+
+        for (var i = 0; i < 44; i++) {
+            this.contextOfCanvas.beginPath();
+            this.contextOfCanvas.moveTo(0, i * this.cellWidth);
+            this.contextOfCanvas.lineTo(this.canvasOfBattleField.width, i * this.cellWidth);
+            this.contextOfCanvas.strokeStyle = 'white';
+            this.contextOfCanvas.stroke();
         }
     }
 }
