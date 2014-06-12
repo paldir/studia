0cf5f8547144beb5642d10db3d27706fbb8146a3
Patryk <patrickus@wp.pl>
2014-06-12 17:49:43 +0200

    Sterowanie + uszczegolowienie siatki

--- spaceInvaders/BattleFieldManagement.js
+++ spaceInvaders/BattleFieldManagement.js
@@ -17,27 +17,49 @@
         var moveDown = false;
 
         if (this.directionOfAttack == 'L') {
-            if (this.invaders[54].locationX == 42) {
+            if (this.invaders[54].locationX[1] == 85) {
                 this.directionOfAttack = 'R';
                 moveDown = true;
             }
         }
         else
-            if (this.invaders[0].locationX == 1) {
+            if (this.invaders[0].locationX[0] == 2) {
                 this.directionOfAttack = 'L';
                 moveDown = true;
             }
 
         if (moveDown)
-            for (var i = 0; i < this.invaders.length; i++)
-                this.invaders[i].locationY++;
+            for (var i = 0; i < this.invaders.length; i++) {
+                this.invaders[i].locationY[0]++;
+                this.invaders[i].locationY[1]++;
+            }
 
         if (this.directionOfAttack == 'L')
             var displacementX = 1;
         else
             var displacementX = -1;
 
-        for (var i = 0; i < this.invaders.length; i++)
-            this.invaders[i].locationX = this.invaders[i].locationX + displacementX;
+        for (var i = 0; i < this.invaders.length; i++) {
+            this.invaders[i].locationX[0] = this.invaders[i].locationX[0] + displacementX;
+            this.invaders[i].locationX[1] = this.invaders[i].locationX[1] + displacementX;
+        }
+    }
+
+    this.MoveHero = function (keyCode) {
+        switch (keyCode) {
+            case 37:
+                if (this.hero.locationX[0] > 0) {
+                    this.hero.locationX[0]--;
+                    this.hero.locationX[1]--;
+                }
+                break;
+            case 39:
+                if (this.hero.locationX[1] < 87) {
+                    this.hero.locationX[0]++;
+                    this.hero.locationX[1]++;
+                }
+                break;
+            default:
+        }
     }
 }
--- spaceInvaders/ShipsAndMissiles.js
+++ spaceInvaders/ShipsAndMissiles.js
@@ -1,10 +1,12 @@
 ﻿function Invader(numberOfInvader, numberOfRow) {
-    this.locationX = (numberOfInvader - 1) * 2 + 1;
-    this.locationY = (numberOfRow - 1) * 2 + 1;
+    //this.locationX = (numberOfInvader - 1) * 2 + 1;
+    this.locationX = new Array((numberOfInvader - 1) * 4 + 2, (numberOfInvader - 1) * 4 + 3);
+    //this.locationY = (numberOfRow - 1) * 2 + 1;
+    this.locationY = new Array((numberOfRow - 1) * 4 + 2, (numberOfRow - 1) * 4 + 3);
     this.destroyed = false;
 }
 
 function Hero() {
-    this.locationX = 21
-    this.locationY = 42;
+    this.locationX = new Array(42, 43);
+    this.locationY = new Array(84, 85);
 }
