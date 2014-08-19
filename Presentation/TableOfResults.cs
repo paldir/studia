d869cdbe495413f3cdac0f622d532583d28af169
Patryk <patrickus@wp.pl>
2014-08-19 20:03:21 +0200

    Przyciski w komorkach

--- Presentation/TableOfResults.cs
+++ Presentation/TableOfResults.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI.WebControls;
 using System.Web.UI;
 
+using System.Drawing;
+
 namespace Presentation
 {
     public class TableOfResults
@@ -16,8 +18,11 @@ namespace Presentation
             Table table = new Table();
             TableRow tableRow;
             TableCell tableCell;
+            Bitmap bitMap = new Bitmap(500, 200);
+            Graphics graphics = Graphics.FromImage(bitMap);
 
             table.ID = "TableOfResults";
+            table.CssClass = "tableOfResults";
             table.BorderWidth = 1;
             table.GridLines = (GridLines)3;
 
@@ -31,8 +36,16 @@ namespace Presentation
 
                     if (description[i, j] != "Value")
                         tableCell.Font.Bold = true;
+                    
+                    Button button = new Button();
+                    button.Width = 15;
+                    button.Height = 15;
+                    button.CssClass = "buttonInsideTableOfResults";
+                    tableCell.Width = new Unit(graphics.MeasureString(arrayOfResults[i, j], new System.Drawing.Font("Arial", 10)).Width + 30);
 
                     tableCell.Controls.Add(new LiteralControl(arrayOfResults[i, j]));
+                    tableCell.Controls.Add(button);
+
                     tableRow.Cells.Add(tableCell);
                 }
 
