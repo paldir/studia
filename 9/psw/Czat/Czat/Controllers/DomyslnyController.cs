de88b4f2616f32ee291f8ed504e1acf2258e4321
Patryk <patrickus@wp.pl>
2016-02-09 22:15:16 +0100

    ...

---
+++ 9/psw/Czat/Czat/Controllers/DomyslnyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Czat.Models;
+
+namespace Czat.Controllers
+{
+    public class DomyslnyController : Controller
+    {
+        public ActionResult Index(DaneLogowania dane = null)
+        {
+            return View(dane);
+        }
+    }
+}
---
+++ 9/psw/Czat/Czat/Controllers/UzytkownikController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Czat.Models;
+
+namespace Czat.Controllers
+{
+    public class UzytkownikController : Controller
+    {
+        private readonly Encje _encje;
+
+        public UzytkownikController()
+        {
+            _encje = new Encje();
+        }
+
+        public ActionResult Logowanie(DaneLogowania dane)
+        {
+            Uzytkownik uzytkownik = _encje.Uzytkownik.SingleOrDefault(u => string.Equals(u.Nazwa, dane.Login) && string.Equals(u.Haslo, dane.Haslo));
+
+            if (uzytkownik == null)
+            {
+                dane.Haslo = null;
+                dane.NieudaneLogowanie = true;
+
+                return RedirectToAction("Index", "Domyslny", new {dane = dane});
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Rejestracja(Uzytkownik uzytkownik)
+        {
+            return View(uzytkownik);
+        }
+    }
+}
